c43bbed0f668190f99f6f84665ad909692fb32b8
marcuspeh <hkmpeh@gmail.com>
2024-03-04 23:48:27 +0800

    Update library to use nimble

--- esp32-split-keyboard.ino
+++ esp32-split-keyboard.ino
@@ -1,7 +1,7 @@
 /**
  * This example turns the ESP32 into a Bluetooth LE keyboard that writes the words, presses Enter, presses a media key and then Ctrl+Alt+Delete
  */
-#include <BleKeyboard.h>
+#include "BleKeyboard.h"
 
 BleKeyboard bleKeyboard;
 
@@ -13,32 +13,15 @@ void setup() {
 
 void loop() {
   if(bleKeyboard.isConnected()) {
-    Serial.println("Sending 'Hello world'...");
-    bleKeyboard.print("Hello world");
-
-    delay(1000);
-
-    Serial.println("Sending Enter key...");
-    bleKeyboard.write(KEY_RETURN);
-
-    delay(1000);
-
-    Serial.println("Sending Play/Pause media key...");
-    bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
-
-    delay(1000);
-
    //
    // Below is an example of pressing multiple keyboard modifiers 
    // which by default is commented out.
-    /*
     Serial.println("Sending Ctrl+Alt+Delete...");
-    bleKeyboard.press(KEY_LEFT_CTRL);
-    bleKeyboard.press(KEY_LEFT_ALT);
-    bleKeyboard.press(KEY_DELETE);
+    bleKeyboard.press(KEY_LEFT_SHIFT);
     delay(100);
     bleKeyboard.releaseAll();
-    */
+  } else {
+    Serial.println("Not connected");
   }
 
   Serial.println("Waiting 5 seconds...");
